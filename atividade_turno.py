c13833d4587a180ce34772bb340e28cfaf15c865
Vrau7 <116606893+Vrau7@users.noreply.github.com>
2022-10-28 19:39:15 -0300

    Atividade tabuada

---
+++ atividade_turno.py
@@ -0,0 +1,11 @@
+#Atividade turno
+
+print("Tabuada do numero desejado")
+tabuada = int(input("Digite o numero da tabuada: "))
+inicio = int(input("Digite o inicio do intervalo da tabuada "))
+fim = int(input("Digite o fim do intervalo da tabuada: "))
+
+for i in range(inicio, fim + 1):
+    print("Tabuada do numero", tabuada, " resultado ", tabuada, "*", inicio,"=", tabuada * inicio)
+    print( "Tabuada resultado: ", tabuada * inicio)
+    inicio = inicio + 1
