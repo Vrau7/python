e1700940f1e1d6791561ba9f8ea6d006bfb995c4
Vrau7 <http.sslbackend>
2022-10-28 19:22:59 -0300

    Atividade turno

--- Atividade_turno.py
+++ Atividade_turno.py
@@ -1,9 +1,16 @@
 #Atividade turno
 
-turno = input("Qual turno voce estuda? Caso seja Matutino digite M, Vespertino digite V e Noturno N: ",)
+turno = input("Qual turno voce estuda? Caso seja Matutino digite M, Vespertino digite V e Noturno N: ").upper()
 
-match (turno):
+match turno:
      case "M":
+          print("Bom dia!")
+     case "T":
+          print("Boa tarde!")
+     case "N":
+          print ("Boa noite!")
+     case _:
+          print("Valor invalido")
 
 
 
